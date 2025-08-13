9e83e968f0430adec0f1faef20dcb850431b7448
Krambrek <225311930+Krambrek@users.noreply.github.com>
2025-08-13 20:43:10 +0300

    Add basic/creating_a_window_4.py

---
+++ basic/creating_a_window_4.py
@@ -0,0 +1,26 @@
+import sys
+
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('My App')
+
+        button = QPushButton('Press Me!')
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
